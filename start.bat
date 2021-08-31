039b7fa96bcc1096a6e0b9f83264fd5b8a585565
gagominecraft12 <gaeliangarza@gmail.com>
2021-08-30 19:16:51 -0500

    funni update lol

--- start.bat
+++ start.bat
@@ -1,22 +1,40 @@
+echo off
+
+
+
+@echo off && cls
+
+title Running Blue'ity Client b0.0.1
+
+TIMEOUT /T 5
+
 :: Important stuff
 @echo off && cls
-title Blue'ity Client
+title Blue'ity Client b0.0.1 (Starting)
 
 ::::::::::::::::::::
 :: Initialization ::
 ::::::::::::::::::::
 
 :: Terminate existing node.js apps
+echo gonna terminate the existing node js apps. so lets get a delay
+
+TIMEOUT /T 2
+
+title Blue'ity Client b0.0.1 (Terminating Existing node apps.)
+
 TASKKILL /IM node.exe /F 2>nul
 cls
 
 :::::::::::::::::::::::::::::
-:: Start Blue'ity Client ::
+::  Start Blue'ity Client  ::
 :::::::::::::::::::::::::::::
 
 :: Check for installation
+@echo GOING TO CHECK FOR 
 
-	
+:start
+	title Blue'ity Client b0.0.1 (Checking)
 
 if exist notinstalled (
 	echo Blue'ity Client not downloaded! Installing...
@@ -28,12 +46,40 @@ if exist notinstalled (
 	goto start
 )
 
+::VFProxy installation
+:start
+
+title Blue'ity Client b0.0.1 (Installing VFProxy)
+
+echo we need to run the VFProxy
+
+echo Dont worry. we need to delay this batch before we wait to install VFProxy
+
+TIMEOUT /T 5
+
+cd .\VFProxy
+
+start start.bat
+
+cd  ..
+
+echo Starting.
+
+TIMEOUT /T 15
+
+title Blue'ity Client b0.0.1 
+
+@echo Lets clear to make it more good
+
+TIMEOUT /t 5
+
+echo off
+
+@echo off && cls
+
 :: Run npm start
 :start
 echo Blue'ity client is now starting...
-echo Running VFProxy..
 
-start tts\VFProxy\start.bat
-
-echo Please navigate to http://localhost on your browser.
+echo Please navigate to http://localhost:1200 on your browser.
 npm start
